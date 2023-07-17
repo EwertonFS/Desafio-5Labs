d101b63e4fc05c4db4cf582fd105edeae8e4bb48
ewerton <ewerton.lost2@gmail.com>
2023-07-16 21:42:23 -0300

    cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,8 +3,13 @@ import cors from "cors"
 
 const app : Express = express()
 
+// Usa o cors com a opção origin definida
+app.use(cors({
+    origin: '*'
+  }));
+
+// Usa o express.json depois do cors
 app.use(express.json())
-app.use(cors())
 
 app.listen(3003 , () => {
     console.log("Server ready!")
